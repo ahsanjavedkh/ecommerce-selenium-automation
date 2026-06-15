e0808eb828fd8788c48aa225d4cffa8f68a9efab
ahsanjavedkh <ahsan1880136@gmail.com>
2026-06-15 14:35:26 +0500

    Add end-to-end checkout test

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from config import EMAIL, PASSWORD
-import time
 
 from pages.login_page import LoginPage
 from pages.products_page import ProductsPage
@@ -11,8 +9,10 @@ from pages.products_page import ProductsPage
 driver = webdriver.Chrome()
 
 driver.maximize_window()
+
 driver.get("https://maelano.com/login")
 
+# Login
 login = LoginPage(driver)
 
 login.login(
@@ -20,7 +20,7 @@ login.login(
     PASSWORD
 )
 
-wait = WebDriverWait(driver,20)
+wait = WebDriverWait(driver, 20)
 
 wait.until(
     EC.url_contains("dashboard")
@@ -29,19 +29,39 @@ wait.until(
 print("Current URL after login:")
 print(driver.current_url)
 
+# Open landing page
 driver.get("https://maelano.com/landingpage")
 
 print("URL after get():")
 print(driver.current_url)
 
-time.sleep(5)
-
+# Create ProductsPage object
 product = ProductsPage(driver)
 
-# Search shirt
+# Search product
 product.search_product("shirt")
+
+# Open first product
 product.open_product()
+
 # Add to cart
 product.add_to_cart()
-input("Press Enter...")
+
+# Open cart
+product.open_cart()
+# checkout
+product.proceed_to_checkout()
+product.proceed_to_payment_method()
+product.fill_recipient_details(
+    "Musa Bajwa",
+    "3369958739"
+)
+
+product.proceed_to_finalize()
+product.place_order()
+
+input("Press Enter to close browser...")
+
 driver.quit()
+
+
